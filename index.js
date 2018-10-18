e68994e1d605c2051eab477222d0b17c3a9c0937
HuynhHien <hqhientius@gmail.com>
2018-10-18 14:30:41 +0700

    new param

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = express();
 const port = process.env.PORT || 3000;
 
 server.get('/register', (req, res) => {
-  console.log(`Requset: ${req.params}!`);  
+  console.log(`Requset: ${req.query.device}!`);  
   res.send("Thanks you !!!");
 });
 
