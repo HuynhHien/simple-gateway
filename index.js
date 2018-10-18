a24beb1c89edc66bad824f740c95ad084ecd5671
HuynhHien <hqhientius@gmail.com>
2018-10-18 14:26:22 +0700

    test input

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = express();
 const port = process.env.PORT || 3000;
 
 server.get('/register', (req, res) => {
-  console.log(`Requset: ${req.params.id}!`);  
+  console.log(`Requset: ${req.params.device}!`);  
   res.send("Thanks you !!!");
 });
 
